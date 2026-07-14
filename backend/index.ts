b94926e3ae7fc0c7889d0fe00efd12e65d0eb8ed
Nitin Patel M <nithinpatelmlm@gmail.com>
2026-07-14 10:19:40 +0530

    Refactor database connection and update User model to enforce avatar field

--- backend/index.ts
+++ backend/index.ts
@@ -6,8 +6,13 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is up and running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is up and running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
   });
-});
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   console.log("Connecting to MongoDB...");
-  console.log(process.env.MONGODB_URI);
+  const mongoUri = process.env.MONGODB_URI;
+  if (!mongoUri) {
+    console.error("MONGODB_URI is not defined in the environment variables.");
+    process.exit(1);
+  }
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(mongoUri);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
